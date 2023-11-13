cfbdac8c20a70c44d4d14be418659896720e2e4b
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-11-13 11:40:28 +0300

    gameEnd

--- src/components/phases/Phase0.tsx
+++ src/components/phases/Phase0.tsx
@@ -1,14 +1,13 @@
 import {Actions} from "../../context/actions"
 import {useAppContext} from "../../context"
 import useFlow from "../../hooks/useFlow"
-import {commonId} from "../../data/players"
-import {Zone} from "../../data/zones"
+// import {commonId} from "../../data/players"
+// import {Zone} from "../../data/zones"
 
 const Phase0 = () => {
   const { state, dispatch } = useAppContext()
-
   const {
-    cards,
+    // cards,
     curTurn,
     players,
   } = state
@@ -21,14 +20,14 @@ const Phase0 = () => {
   const phaseEnd = players.every(p => p.pass)
 
   const handlePlay = () => {
-    console.log(`- Turn: ${curTurn}`)
-    const drawPile = cards.filter(card => card.idPlayer === commonId && card.idZone === Zone.DrawPile)
-    if (drawPile.length) {
-      const card = {...drawPile[0]}
-      card.idPlayer = players.at(curTurn).id
-      card.idZone = Zone.Hand
-      dispatch({type: Actions.DrawCard, payload: card})
-    }
+    console.log(`- Play: ${curTurn}`)
+    // const drawPile = cards.filter(card => card.idPlayer === commonId && card.idZone === Zone.DrawPile)
+    // if (drawPile.length) {
+    //   const card = {...drawPile[0]}
+    //   card.idPlayer = players.at(curTurn).id
+    //   card.idZone = Zone.Hand
+    //   dispatch({type: Actions.DrawCard, payload: card})
+    // }
 
     const rnd = Math.floor(Math.random() * 25 + 1)
     dispatch({type: Actions.IncValue, payload: {idx: curTurn, value: rnd}})
--- src/components/phases/Phase3.tsx
+++ src/components/phases/Phase3.tsx
@@ -1,23 +1,37 @@
+import {useAppContext} from "../../context"
 import useFlow from "../../hooks/useFlow"
+import {commonId} from "../../data/players"
+import {Zone} from "../../data/zones"
 
 const Phase3 = () => {
+  const { state } = useAppContext()
   const {
-    handleNextHandPhase,
+    cards,
+  } = state
+
+  const deck = cards.filter(card => card.idPlayer === commonId && card.idZone === Zone.DrawPile)
+  const gameEnd = !deck.length
+
+  const {
     handleNextHand,
     handleEndGame,
   } = useFlow()
 
   return (
     <>
-      <button onClick={() => handleNextHandPhase(0)}>
-        Next Phase
-      </button>
-      <button onClick={handleNextHand}>
-        Next Hand
-      </button>
-      <button onClick={handleEndGame}>
-        End Game
-      </button>
+      <h2>{`${gameEnd?"Game End":"*"}`}</h2>
+      {
+        gameEnd?
+          <>
+            <button onClick={handleEndGame}>
+              End Game
+            </button>
+          </>:<>
+            <button onClick={handleNextHand}>
+              Next Hand
+            </button>
+          </>
+      }
     </>
   )
 }
