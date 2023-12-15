237e0f5072e440dcfcbea6ddb8662036f64ddaa1
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-12-16 00:31:45 +0300

    Abil upd

--- src/data/abilities.ts
+++ src/data/abilities.ts
@@ -1,36 +1,54 @@
 export enum Ability {
   Empty = "_",
+  Burrowing = "BUR",
+  Camouflage = "CMF",
   Carnivore = "CRN",
   Grazing = "GRZ",
   Hibernation = "HIB",
+  HighBodyWeight = "HBW",
+  Parasite = "parasite",
   Piracy = "PIR",
+  SharpVision = "SHV",
+  Swimming = "SWI",
 }
 
 export type TAbility =
   | Ability.Empty
+  | Ability.Burrowing
+  | Ability.Camouflage
   | Ability.Carnivore
   | Ability.Grazing
   | Ability.Hibernation
+  | Ability.HighBodyWeight
+  | Ability.Parasite
   | Ability.Piracy
+  | Ability.SharpVision
+  | Ability.Swimming
 
 interface IAbility{
   active?: boolean
 }
 
 const abilityMap = new Map<TAbility, IAbility>([
+  [Ability.Burrowing, {}],
+  [Ability.Camouflage, {}],
   [Ability.Carnivore, {active: true}],
   [Ability.Grazing, {active: true}],
   [Ability.Hibernation, {active: true}],
+  [Ability.HighBodyWeight, {}],
+  [Ability.Parasite, {}],
   [Ability.Piracy, {active: true}],
+  [Ability.SharpVision, {}],
+  [Ability.Swimming, {}],
 ])
 
 export const isActive = (id: TAbility) => !!abilityMap.get(id)?.active
 
 export const getSpellId =
   (idx: number): TAbility => (idx % 2) ?
-    Ability.Piracy
-    : Ability.Hibernation
-    // [Ability.Carnivore, Ability.Grazing, Ability.Hibernation, Ability.Piracy][idx % 4]
+    Ability.Carnivore
+    // : Ability.Camouflage
+    : [Ability.Burrowing, Ability.Camouflage, Ability.SharpVision][idx % 3]
     // : Ability.Empty
 
 export const isEmpty = (spellId: TAbility) => Ability.Empty === spellId
